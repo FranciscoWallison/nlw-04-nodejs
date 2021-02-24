3c3cea1df8281906c8bbf1b2446e083a92d7825a
Francisco Wallison <franciscowallison@gmail.com>
2021-02-24 09:53:53 -0300

    adicionando database de tests

--- dbs/init/nlw_mysql.sql
+++ dbs/init/nlw_mysql.sql
@@ -1,1 +1,2 @@
-create database database_nlw;
+-- create database database_nlw;
+create database database_nlw_test;
--- dbs/init/nlw_pg.sql
+++ dbs/init/nlw_pg.sql
@@ -1,3 +1,2 @@
-create database database_nlw;
-
-\c database_nlw
+-- create database database_nlw;
+create database database_nlw_test;
